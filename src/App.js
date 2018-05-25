03d49679bb70561a2a05997f687873217c843472
dee <dee@adadevelopersacademy.org>
2018-05-25 15:14:07 -0700

    adding name and email and grade props into Student, iterating through student data

--- src/App.js
+++ src/App.js
@@ -5,19 +5,41 @@ import Student from './components/Student';
 
 class App extends Component {
   render() {
-    const myName = "Kari";
+    // we can render an array of JSX if we want.
+    // for example, adding {someStudents} below, this will work
+    const someStudents = [
+    ( <Student key='f' name='new student' email='f' grade='g' /> ),
+    ( <Student key='f' name='another student' email='h' grade='i' /> )];
+
+    // just an array of JS objects...
+    const studentData = [
+      {
+        name: 'Ada',
+        email: 'ada@ada.co'
+      },
+      {
+        name: 'Grace',
+        email: 'grace@ada.co'
+      }
+    ];
+
+    // an unfinished foreach loop... this loop does nothing, but shows us we can use a forEach loop in all of the ways we're familiar with :)
+    studentData.forEach( (student) => {
+      student.name;
+      student.email;
+    } );
+
+    // we can make an array of JSX via iterating through a set of data
+    const studentComponents = studentData.map((student, index) => {
+      return <Student key={ index } name={ student.name } email={ student.email } />
+    });
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <h1>Student List:</h1>
 
-        <Student />
+        { studentComponents }
 
-        <p>{ myName }</p>
-        {/*This is a comment example*/}
       </div>
     );
   }
--- src/components/Student.js
+++ src/components/Student.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 class Student extends Component {
   render() {
+    const studentName = this.props.name;
+    const studentEmail = this.props.email;
+
     return (
       <section>
-        <strong>Name: </strong>
-        YOUR NAME HERE
+        <ul>
+          <li>Name: { studentName } </li>
+          <li>Email: { studentEmail }</li>
+          <li>Grade: { this.props.grade }</li>
+        </ul>
       </section>
     );
   }
